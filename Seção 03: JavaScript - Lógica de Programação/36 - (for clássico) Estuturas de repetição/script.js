6fa6013d0de0310af25be7e60d86753b069bfe1b
Robson Santana Soares <robson.santana.soares@gmail.com>
2021-02-28 12:44:16 -0300

    adding file

---
+++ Seção 03: JavaScript - Lógica de Programação/36 - (for clássico) Estuturas de repetição/script.js
@@ -0,0 +1,10 @@
+const fruits = ['maçã', 'pêra', 'uva'];
+
+for(let i = 0;  i < fruits.length; i++) {
+    console.log(`índicie ${i}:`, fruits[i]);
+}
+
+
+
+// const even = i % 2 == 0 ? `${i} é par` : `${i} é ímpar`;
+// console.log(even);
