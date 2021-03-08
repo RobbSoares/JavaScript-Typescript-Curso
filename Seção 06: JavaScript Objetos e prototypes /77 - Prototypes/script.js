c886e8a310aa1083554c47c6d02d106ec0eaa5b1
Robson Soares <robson.santana.soares@gmail.com>
2021-03-08 17:00:16 -0300

    uploading files

--- Seção 06: JavaScript Objetos e prototypes /77 - Prototypes/script.js
+++ Seção 06: JavaScript Objetos e prototypes /77 - Prototypes/script.js
@@ -4,7 +4,7 @@ JavaScript é baseado em protótipos para passar propriedades e metódos de um o
 Definição de protótipo
 Protótipo é o termo usado para se referir ao que foi criado pela primeira vez, servindo de modelo ou molde para futuras produções.
 
-Todos os objetos tem uma referência interna para um protótipo (__proto__) que vem da propriedade prototype da função construtora que foi usada para criá-lo. Quando tentamos acessar um membro de um objeto, primeiro o motor do JS vai tentar encontrar este membro no próprio objeto e depois a cadeia de protótipos é usada até o topo (null) até encontrar (ou não) tal membro. */
+Todos os objetos tem uma referência interna para um protótipo (___proto___) que vem da propriedade prototype da função construtora que foi usada para criá-lo. Quando tentamos acessar um membro de um objeto, primeiro o motor do JS vai tentar encontrar este membro no próprio objeto e depois a cadeia de protótipos é usada até o topo (null) até encontrar (ou não) tal membro. */
 
 
 function Person(name, surname) {
@@ -13,6 +13,8 @@ function Person(name, surname) {
 
     this.fullName = () => this.name + ' ' + this.surname;
 }
+
+const personOne = new Person('Rob', 'Soares');
 
 
 
