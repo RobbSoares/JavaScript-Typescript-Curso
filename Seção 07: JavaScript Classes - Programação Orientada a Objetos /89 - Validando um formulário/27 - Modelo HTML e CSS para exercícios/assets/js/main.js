cccbc2ea1047dc9b4fec6c042e90c548d03a5bc9
Robson Soares <robson.santana.soares@gmail.com>
2021-03-13 20:36:30 -0300

    uploading files

--- Seção 07: JavaScript Classes - Programação Orientada a Objetos /89 - Validando um formulário/27 - Modelo HTML e CSS para exercícios/assets/js/main.js
+++ Seção 07: JavaScript Classes - Programação Orientada a Objetos /89 - Validando um formulário/27 - Modelo HTML e CSS para exercícios/assets/js/main.js
@@ -13,6 +13,32 @@ class ValidateForm {
     handleSubmit(e) {
         e.preventDefault();
         const validFields = this.checkFields();
+        const validPassword = this.checkPassword();
+
+        if(validFields && validPassword) {
+            alert('Formulário enviado.');
+            this.form.submit();
+        }
+    }
+
+    checkPassword() {
+        let valid = true;
+
+        const password = this.form.querySelector('.password');
+        const repeatPassword = this.form.querySelector('.confirm-password');
+
+        if(password.value !== repeatPassword.value) {
+            valid = false;
+            this.createError(password, 'Campos senha e repetir senha precisam ser iguais');
+            this.createError(repeatPassword, 'Campos senha e repetir senha precisam ser iguais');
+        }
+
+        if(password.value.length < 6 || password.value.length > 12) {
+            valid = false;
+            this.createError(repeatPassword, 'Senha precisa estar entre 6 e 12 caracteres');
+        }
+
+        return valid;
     }
     
     checkFields() {
@@ -27,7 +53,46 @@ class ValidateForm {
                 this.createError(field, `Campo "${label}" não pode estar em branco.`);
                 valid = false;
             }
+            if(field.classList.contains('cpf')) {
+                if(!this.validateCPF(field)) {
+                    valid = false;
+                }
+            }
+
+            if(field.classList.contains('user')) {
+                if(!this.validateUser(field)) {
+                    valid = false;
+                }
+            }
         }
+
+        return valid;
+    }
+
+    validateUser(field) {
+        const user = field.value;
+        let valid = true;
+
+        if(user.length < 3 || user.length > 12){
+            this.createError(field, 'Usuário precisa ter entre 3 e 12 caracteres.');
+            valid = false;
+        }
+        
+        if(!user.match(/[a-zA-Z0-9]+$/g)){
+            this.createError(field, 'Nome de usuário precisa conter apenas letras e/ou números.');
+            valid = false;
+        }
+
+        return true;
+    }
+
+    validateCPF(field) {
+        const CPF = new ValidateCPF(field.value);
+        if(!CPF.validate()) {
+            this.createError(field, 'CPF Inválido');
+        }
+
+        return true;
     }
 
     createError(field, message) {
